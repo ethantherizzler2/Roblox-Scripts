60d326c48fc295037060c52f7fe53ec0d65ade57
Ethan <ethandiddy33@gmail.com>
2026-03-03 05:38:43 +0100

    Update nyula.lua

--- nyula.lua
+++ nyula.lua
@@ -1,18 +1,2 @@
-local StarterGui = game:GetService("StarterGui")
-
-StarterGui:SetCore("SendNotification", {
-    Title = "Cleint",
-    Text = "wait before i tickle yo feet",
-    Duration = 5
-})
-
-task.wait(0.5)
-StarterGui:SetCore("SendNotification", {
-    Title = "Ethan",
-    Text = "alright its loaded but ITS NEW SO SHUT TEH FEK Up",
-    Duration = 5
-})
-
+-- load this using the AC bypass becuase its detcteced in some games!
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/606b846e88998801018fae498b9b8a3c.lua"))()
-
-print("Loaded!!!!")
